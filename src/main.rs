ced67ed65f84354ac837d3e33e4937b141b6f92e
Anant Narayan <anantnrg@gmail.com>
2023-07-30 20:36:56 +0530

    :sparkles: Added function. Pls help im losing my mind over parsing lua

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-	println!("Hello, world!");
+use mlua::{Lua, Result, UserData, UserDataMethods};
+use std::process::Command;
+
+// Define the userdata for Strata
+struct StrataState;
+
+impl UserData for StrataState {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("spawn", |_, this, cmd: String| Ok(this.spawn(cmd)));
+    }
+}
+
+impl StrataState {
+    fn spawn(&self, cmd: String) {
+        Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .spawn()
+            .expect("Failed to execute the command");
+    }
+}
+
+fn main() -> Result<()> {
+    let lua = Lua::new();
+
+    let strata = StrataState;
+    lua.globals().set("strata", lua.create_userdata(strata)?)?;
+
+    let lua_script = r#"
+        strata:spawn("echo Hello from Lua!")
+    "#;
+
+    lua.load(lua_script).exec()?;
+
+    Ok(())
 }
