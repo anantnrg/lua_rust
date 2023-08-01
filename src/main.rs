2033a70b014fd86226c3e1ad1cdc7ff4363ca23d
Anant Narayan <anantnrg@gmail.com>
2023-08-01 22:51:53 +0530

    :sparkles: Added basic stuff

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,37 @@
-use mlua::{Lua, Result};
+use mlua::{
+	prelude::*,
+	Lua,
+	Result,
+};
 use std::{
 	env::var,
 	fs::read_to_string,
 };
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Config {
-    autostart: Vec<Vec<String>>,
-    general: GeneralConfig,
-    decorations: DecorationsConfig,
-    tiling: TilingConfig,
-    animations: AnimationsConfig,
-    rules: RulesConfig,
-    bindings: Vec<BindingConfig>,
+struct StrataState;
+
+impl StrataState {
+	fn spawn_terminal() -> String {
+		"kitty --title Terminal".to_string()
+	}
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct GeneralConfig {
-    workspaces: u32,
-    gaps_in: u32,
-    gaps_out: u32,
-    kb_repeat: Vec<u32>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct DecorationsConfig {
-    border: BorderConfig,
-    window: WindowConfig,
-    blur: BlurConfig,
-    shadow: ShadowConfig,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct BorderConfig {
-    width: u32,
-    active: String,
-    inactive: String,
-    radius: u32,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct WindowConfig {
-    opacity: f32,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct BlurConfig {
-    enabled: bool,
-    size: u32,
-    passes: u32,
-    optimize: bool,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct ShadowConfig {
-    enabled: bool,
-    size: u32,
-    blur: u32,
-    color: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct TilingConfig {
-    layout: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct AnimationsConfig {
-    enabled: bool,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct RulesConfig {
-    workspaces: Vec<WorkspaceRuleConfig>,
-    floating: Vec<FloatingRuleConfig>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct WorkspaceRuleConfig {
-    workspace: u32,
-    class_name: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct FloatingRuleConfig {
-    class_name: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct BindingConfig {
-    keys: Vec<String>,
-    cmd: String,
-}
+impl LuaUserData for StrataState {}
 
 fn main() -> Result<()> {
-    let lua = Lua::new();
+	let lua = Lua::new();
 	let config_path =
 		format!("{}/.config/strata/strata.lua", var("HOME").expect("This should always be set!!!"));
 	let config_str = read_to_string(config_path).unwrap();
+	let strata_state = lua.create_userdata(StrataState)?;
 
-    lua.load(&config_str).exec()?;
+    lua.globals().set("strata", strata_state)?;
 
-	let config: mlua::Table = lua.globals().get("config")?;
+	lua.load(&config_str).exec().unwrap();
 
-	println!("{:#?}", config);
+    let config = lua.globals().get::<_, LuaTable>("config").unwrap();
+    println!("{:#?}", config);
 
-    Ok(())
+
+	Ok(())
 }
-
