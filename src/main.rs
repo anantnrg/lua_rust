95471dcd7f9e1cc499b0d8a2b72e6083b7e11411
Anant Narayan <anantnrg@gmail.com>
2023-08-02 14:24:18 +0530

    :sparkles: Added function calling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use mlua::{
 	prelude::*,
 	Lua,
 	Result,
+	Table,
+	UserData,
+	Value,
 };
 use std::{
 	env::var,
@@ -11,27 +14,53 @@ use std::{
 struct StrataState;
 
 impl StrataState {
-	fn spawn_terminal() -> String {
-		"kitty --title Terminal".to_string()
+	pub fn spawn() {
+		println!("kitty --title Terminal");
 	}
 }
 
-impl LuaUserData for StrataState {}
+impl UserData for StrataState {}
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
 	let lua = Lua::new();
 	let config_path =
 		format!("{}/.config/strata/strata.lua", var("HOME").expect("This should always be set!!!"));
 	let config_str = read_to_string(config_path).unwrap();
-	let strata_state = lua.create_userdata(StrataState)?;
 
-    lua.globals().set("strata", strata_state)?;
+	let state_mod = get_or_create_module(&lua, "strata")?;
 
-	lua.load(&config_str).exec().unwrap();
+	let spawn = lua.create_function(|_, _: ()| {
+		Ok(StrataState::spawn())
+	})?;
 
-    let config = lua.globals().get::<_, LuaTable>("config").unwrap();
-    println!("{:#?}", config);
+	state_mod.set("spawn", spawn)?;
 
+	lua.load(&config_str).exec()?;
 
 	Ok(())
+}
+
+pub fn get_or_create_module<'lua>(lua: &'lua Lua, name: &str) -> anyhow::Result<mlua::Table<'lua>> {
+	let globals = lua.globals();
+	let package: Table = globals.get("package")?;
+	let loaded: Table = package.get("loaded")?;
+
+	let module = loaded.get(name)?;
+	match module {
+		Value::Nil => {
+			let module = lua.create_table()?;
+			loaded.set(name, module.clone())?;
+			Ok(module)
+		}
+		Value::Table(table) => Ok(table),
+		wat => {
+			anyhow::bail!(
+				"cannot register module {} as package.loaded.{} is already set to a value of type \
+				 {}",
+				name,
+				name,
+				wat.type_name()
+			)
+		}
+	}
 }
